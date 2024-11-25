8ecc73f8347d6783ae29e85f338689b72c7333e5
lajunastudio <lajunastudio@gmail.com>
2024-11-26 00:46:06 +0700

    Create banner-loading.js

---
+++ xavier/banner-loading.js
@@ -0,0 +1,10 @@
+
+	atOptions = {
+		'key' : '1e8cfb88a9b5827f12642a9098d2b752',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/1e8cfb88a9b5827f12642a9098d2b752/invoke.js"></scr' + 'ipt>');
+
